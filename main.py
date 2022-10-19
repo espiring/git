9e8df04bab713a5bc9b32ae2aadee9c88d38068c
espiring <53615735+espiring@users.noreply.github.com>
2022-10-19 12:26:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-for x in range(10):
-	print(x)
+import os
+import platform as pl
+
+
+
+def get_os():
+	try:]
+		check1 = os.name
+		check2 = pl.system()
+		answers = {
+			"windows": ["nt", "Windows"]
+			"linux": ["Linux"]
+		
+		if(check1 in answers['windows'][0] and check2 in answers['linux'][1]):
+			return "Windows"
